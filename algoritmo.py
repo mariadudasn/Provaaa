c805c0fa731f53272321d365a81213eefe45adf9
mariadudasn <maria.santos081207@gmail.com>
2023-08-31 09:15:28 -0300

    Update

---
+++ algoritmo.py
@@ -0,0 +1,65 @@
+from classes import *
+import os
+
+def main ():
+    contTarefas = 0
+    lista = ToDoList() 
+
+    sair = False
+    while sair == False:
+        try:
+            os.system ("cls")
+            print("---MENU - ToDoList---")
+            print("01 - ADICIONAR TAREFAS")
+            print("02 - EXCLUIR TAREFAS")
+            print("03 - LISTAR TAREFAS")
+            print("00 - SAIR")
+            print("--------")
+            print (" ")
+
+            print ("Qual opção deseja?")
+            menu = int(input(">> "))
+            os.system ("pause")
+
+            match menu:
+                case 1:
+                    os.system ("cls")
+                    print ("---ADICIONAR TAREFAS---")
+                    contTarefas += 1
+                    indice = contTarefas
+                    descricao = input ("Qual é a tarefa? - ")
+
+                    lista.adiconar_tarefa(indice, descricao)
+                    print ("Tarefa adicionada")
+                    os.system ("pause")
+
+                case 2:
+                    os.system("cls")
+                    print ("---LISTA DE TAREFAS---")
+                    lista.listar_tarefas()
+                    os.system ("pause")
+
+                    indice = int(input("Qual tarefa deseja excluir? Digite o indice: \n >> "))
+                    lista.excluir_tarefa(indice)
+                    os.system ("pause")
+
+                case 3:
+                    os.system("cls")
+                    print ("---LISTA DE TAREFAS---")
+                    lista.listar_tarefas()
+                    os.system ("pause")
+
+                case 0:
+                    print ("SAINDO...")
+                    print ("------")
+                    sair = True
+
+                case _:
+                    print("Valor inválido")
+                    print ("-------")
+
+        except Exception as erro:
+            print ("Valor inválido")
+            print (erro.__class__.__name__)
+            print(" ")
+            os.system("pause")
---
+++ classes.py
@@ -0,0 +1,20 @@
+class ToDoList:
+    def __init__(self):
+        self.tarefas = {}
+
+    def adiconar_tarefa (self, indice, descricao):
+        self.indice = indice
+        self.descricao = descricao
+        self.tarefas[self.indice] = [self.descricao]
+
+    def excluir_tarefa (self, indice):
+        self.indice = indice
+        for indice in self.tarefas.items():
+            del self.tarefas[self.indice]
+            print ("Tarefa excluída")
+
+            
+    def listar_tarefas(self):
+        for chave,valor in self.tarefas.items():
+            print (f"{chave} - Tarefa: {valor [0]}")
+
